66a8af8a1d219f7cc70e7ca3c05fda6a95233e12
Claire Bailey-Swift <claire.baileyswift@concentrix.com>
2023-02-28 14:09:45 +0000

    Ticket 4 - Capitalise User Name Fields

--- python-survey-app/test_csv_reader.py
+++ python-survey-app/test_csv_reader.py
@@ -48,6 +48,37 @@ def test_remove_duplicates():
     assert new_data[1]['answer_1'] == 'yes'
     assert new_data[1]['answer_2'] == 'b'
     assert new_data[1]['answer_3'] == '7'
+    
+def test_read_csv_file_with_capitalized_names():
+    # Arrange
+    expected_data = [
+        {'user_id': '1', 'first_name': 'Charissa', 'last_name': 'Clark', 'answer_1': 'yes', 'answer_2': 'c', 'answer_3': '7'},
+        {'user_id': '2', 'first_name': 'Richard', 'last_name': 'Mckinney', 'answer_1': 'yes', 'answer_2': 'b', 'answer_3': '7'},
+        {'user_id': '3', 'first_name': 'Patience', 'last_name': 'Reeves', 'answer_1': 'yes', 'answer_2': 'b', 'answer_3': '9'},
+        {'user_id': '4', 'first_name': 'Harding', 'last_name': 'Estrada', 'answer_1': 'no', 'answer_2': 'b', 'answer_3': '14'},
+        {'user_id': '5', 'first_name': 'India', 'last_name': 'Gentry', 'answer_1': 'yes', 'answer_2': 'c', 'answer_3': '7'},
+        {'user_id': '6', 'first_name': 'Abra', 'last_name': 'Sheppard', 'answer_1': 'yes', 'answer_2': 'b', 'answer_3': '6'},
+        {'user_id': '6', 'first_name': 'Abra', 'last_name': 'Sheppard', 'answer_1': 'no', 'answer_2': 'a', 'answer_3': '8'},
+        {'user_id': '7', 'first_name': 'Bryar', 'last_name': 'Cooley', 'answer_1': 'yes', 'answer_2': 'a', 'answer_3': '11'},
+        {'user_id': '8', 'first_name': 'Diana', 'last_name': 'Cameron', 'answer_1': 'yes', 'answer_2': 'b', 'answer_3': '9'},
+        {'user_id': '9', 'first_name': 'Alexander', 'last_name': 'Herring', 'answer_1': 'no', 'answer_2': 'b', 'answer_3': '4'},
+        {'user_id': '10', 'first_name': 'Graiden', 'last_name': 'Cannon', 'answer_1': 'yes', 'answer_2': 'a', 'answer_3': '10'}
+    ]
+    input_file = 'test_data.csv'
+    
+    # Act
+    actual_data = read_csv_file(input_file)
+    
+    # Assert
+    assert actual_data == expected_data
+    
+    # Check if first_name and last_name are capitalized in actual_data
+    for row in actual_data:
+        assert row['first_name'].isupper() == False  # make sure not all upper case
+        assert row['last_name'].isupper() == False  # make sure not all upper case
+        assert row['first_name'][0].isupper() == True  # check if first letter is capitalized
+        assert row['last_name'][0].isupper() == True  # check if first letter is capitalized
+
 
 if __name__ == '__main__':
     input_file = 'results.csv'
